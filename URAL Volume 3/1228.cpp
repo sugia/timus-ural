50763bc86b7ebd677908bb0054d1fbe66aa17b8f
Sugia Lau <lausugia@gmail.com>
2013-09-30 23:49:53 +0800

    create 1228.cpp

---
+++ URAL Volume 3/1228.cpp
@@ -0,0 +1,22 @@
+//#include "stdafx.h"
+#include <iostream>
+#include<stdio.h>
+ 
+int main()
+{ 
+    long long n, s, a, b;
+	scanf("%lld%lld", &n, &s);
+	scanf("%lld", &a);
+	printf("%lld", s/a-1);
+		--n;
+	while(n>0)
+	{
+		scanf("%lld", &b);
+		printf(" %lld", a/b-1);
+		a=b;
+		--n;
+	}
+	printf("\n");
+	return 0;
+
+}
