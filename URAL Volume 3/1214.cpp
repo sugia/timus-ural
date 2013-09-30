50bea0fcdb5538aa8b7179fb1fc9ecdd580bb4a0
Sugia Lau <lausugia@gmail.com>
2013-09-30 23:47:09 +0800

    create 1214.cpp

---
+++ URAL Volume 3/1214.cpp
@@ -0,0 +1,19 @@
+//#include "stdafx.h"
+#include <stdio.h>
+
+
+int main()
+{
+	long x, y;
+	scanf("%ld %ld", &x, &y);
+	if(x<=0 || y<=0)
+		printf("%ld %ld\n", x, y);
+	else
+	{
+		if(x%2 != y%2)
+			printf("%ld %ld\n", y, x);
+		else
+			printf("%ld %ld\n", x, y);
+	}
+	return 0;
+}
