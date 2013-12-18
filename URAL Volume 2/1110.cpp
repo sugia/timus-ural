112dc47f0e6b9f0a4b261eed42be57b6dd1d9f8e
Sugia Lau <lausugia@gmail.com>
2013-12-18 11:27:13 +0800

    Update 1110.cpp

--- URAL Volume 2/1110.cpp
+++ URAL Volume 2/1110.cpp
@@ -6,35 +6,27 @@ using namespace std;
 #define maxn 1022
 int n, m, y, x, len=0, a[maxn];
 
-inline int sqr(int x)
-{
+inline int sqr(int x){
 	return x*x;
 }
 
-inline int mod(int x, int n, int m)
-{
-	if(n==0)
-		return 1;
+inline int mod(int x, int n, int m){
+	if(n==0) return 1;
 	
-	if(n==1)
-		return x%m;
+	if(n==1) return x%m;
 
-	if(n%2==0)
-		return sqr(mod(x, n/2, m)) % m;
-	else
-		return sqr(mod(x, n/2, m)) %m * x % m;
+	if(n%2==0) return sqr(mod(x, n/2, m)) % m;
+	else return sqr(mod(x, n/2, m)) %m * x % m;
 }
 
-int main()
-{
+int main(){
 
 	scanf("%d%d%d", &n, &m, &y);
 	for(int i=0;i<m;++i)
 		if(mod(i, n, m)==y)
 			a[++len]=i;
 
-	if(len==0)
-	{
+	if(len==0){
 		printf("-1\n");
 		return 0;
 	}
