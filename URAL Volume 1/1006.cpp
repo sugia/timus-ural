33fcd11c2aff1a505350f9392871fc69ddddd11a
Sugia Lau <lausugia@gmail.com>
2013-12-21 22:46:55 +0800

    Update 1006.cpp

--- URAL Volume 1/1006.cpp
+++ URAL Volume 1/1006.cpp
@@ -11,25 +11,21 @@ char smap[M + 1][N + 1];
 int sq[15][3];
 int ans = -1;
  
-void init()
-{
+void init(){
 	for(int i = 0 ; i < M ; i++)
 		cin >> smap[i];
 }
  
-bool check(int ii, int jj, int k)
-{
+bool check(int ii, int jj, int k){
 	if(smap[ii][jj] != ',' && smap[ii][jj] != LU) return false;
 	if(smap[ii][jj + k - 1] != ',' && smap[ii][jj + k - 1] != RU) return false;
 	if(smap[ii + k - 1][jj] != ',' && smap[ii + k - 1][jj] != LB) return false;
 	if(smap[ii + k - 1][jj + k - 1] != ',' && smap[ii + k - 1][jj + k - 1] != RB) return false;
-	for(int i = ii + 1 ; i < ii + k - 1 ; i++)
-	{
+	for(int i = ii + 1 ; i < ii + k - 1 ; i++){
 		if(smap[i][jj] != ',' && smap[i][jj] != V) return false;
 		if(smap[i][jj + k - 1] != ',' && smap[i][jj + k - 1] != V) return false;
 	}
-	for(int j = jj + 1 ; j < jj + k - 1 ; j++)
-	{
+	for(int j = jj + 1 ; j < jj + k - 1 ; j++){
 		if(smap[ii][j] != ',' && smap[ii][j] != H) return false;
 		if(smap[ii + k - 1][j] != ',' && smap[ii + k - 1][j] != H) return false;
 	}
@@ -41,8 +37,7 @@ bool check(int ii, int jj, int k)
 	return flag;
 }
  
-void clear(int ii, int jj, int k)
-{
+void clear(int ii, int jj, int k){
 	for(int i = ii ; i <= ii + k - 1 ; i++)
 		smap[i][jj] = smap[i][jj + k - 1] = ',';
 	for(int j = jj ; j <= jj + k - 1 ; j++)
@@ -52,21 +47,17 @@ void clear(int ii, int jj, int k)
 	cout << endl;*/
 }
  
-void work()
-{
+void work(){
 	int i, j, k, i1, j1, k1;
 	bool flag = true;
-	while(flag)
-	{
+	while(flag){
 		flag = false;
 		for(k = M ; k >= 2 ; k--)
 			for(i = 0 ; i < M - k + 1 ; i++)
-				for(j = 0 ; j < N - k + 1 ; j++)
-				{
+				for(j = 0 ; j < N - k + 1 ; j++){
 					if(i == 6 && j == 11 && k == 7) 
 						k = 7;	
-					if(check(i, j, k)) 
-					{
+					if(check(i, j, k)){
 						ans++;
 						flag = true;
 						sq[ans][0] = i, sq[ans][1] = j, sq[ans][2] = k;
@@ -81,8 +72,7 @@ void work()
 		cout << sq[i][1] << ' ' << sq[i][0] << ' ' << sq[i][2] << endl;
 }
  
-int main()
-{
+int main(){
 	init();
 	work();
 	return 0;
