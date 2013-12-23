58141a096721712bea848d34f92914d82df079c6
Sugia Lau <lausugia@gmail.com>
2013-12-23 12:15:08 +0800

    Update 1026.cpp

--- URAL Volume 1/1026.cpp
+++ URAL Volume 1/1026.cpp
@@ -10,17 +10,12 @@ const int maxn=100022;
 int a[maxn], n, num;
 string str;
 
-void qs(int s, int t)
-{
+void qs(int s, int t){
 	int i=s, j=t, x=a[(s+t)/2], tmp;
-	while(i<=j)
-	{
-		while(a[i]<x)
-			++i;
-		while(x<a[j])
-			--j;
-		if(i<=j)
-		{
+	while(i<=j){
+		while(a[i]<x) ++i;
+		while(x<a[j]) --j;
+		if(i<=j){
 			tmp=a[i];
 			a[i]=a[j];
 			a[j]=tmp;
@@ -28,25 +23,19 @@ void qs(int s, int t)
 			--j;
 		}
 	}
-	if(i<t)
-		qs(i, t);
-	if(s<j)
-		qs(s, j);
+	if(i<t)	qs(i, t);
+	if(s<j)	qs(s, j);
 }
-int main()
-{
+int main(){
 	cin>>n;
-	for(int i=1;i<=n;++i)
-		cin>>a[i];
+	for(int i=1;i<=n;++i) cin>>a[i];
 	
 	qs(1, n);
 	cin>>str;
 	cin>>n;
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		cin>>num;
 		cout<<a[num]<<endl;
 	}
-
 	return 0;
 }
