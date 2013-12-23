a0be03f44f0be01bdee6c134380c4dcb20d30bdf
Sugia Lau <lausugia@gmail.com>
2013-12-23 23:48:04 +0800

    Update 1031.cpp

--- URAL Volume 1/1031.cpp
+++ URAL Volume 1/1031.cpp
@@ -6,17 +6,14 @@ using namespace std;
 #define maxint 1e9
 int d[maxn]={0}, f[maxn];
 
-int main()
-{
-	for(int i=0;i<maxn;++i)
-		f[i]=maxint;
+int main(){
+	for(int i=0;i<maxn;++i)	f[i]=maxint;
 	int n, l1, l2, l3, c1, c2, c3, s, t;
 	scanf("%d%d%d%d%d%d", &l1, &l2, &l3, &c1, &c2, &c3);
 	scanf("%d", &n);
 	scanf("%d%d", &s, &t);
 	
-	if(s>t)
-	{
+	if(s>t){
 		int tmp=s;
 		s=t;
 		t=tmp;
@@ -27,18 +24,12 @@ int main()
 
 
 	f[s]=0;
-	for(int i=s;i<t;++i)
-	{
+	for(int i=s;i<t;++i){
 		int j=1;
-		while(d[i+j]-d[i]<=l3)
-		{
-			if(d[i+j]-d[i]<=l1)
-				f[i+j]=min(f[i+j], f[i]+c1);
-			if(d[i+j]-d[i]<=l2)
-				f[i+j]=min(f[i+j], f[i]+c2);
-			if(d[i+j]-d[i]<=l3)
-				f[i+j]=min(f[i+j], f[i]+c3);
-
+		while(d[i+j]-d[i]<=l3){
+			if(d[i+j]-d[i]<=l1) f[i+j]=min(f[i+j], f[i]+c1);
+			if(d[i+j]-d[i]<=l2) f[i+j]=min(f[i+j], f[i]+c2);
+			if(d[i+j]-d[i]<=l3) f[i+j]=min(f[i+j], f[i]+c3);
 			++j;
 		}
 	}
