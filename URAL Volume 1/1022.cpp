c4a21bef3d6ce679292392d53b26de3a389c9c5b
Sugia Lau <lausugia@gmail.com>
2013-12-23 12:12:55 +0800

    Update 1022.cpp

--- URAL Volume 1/1022.cpp
+++ URAL Volume 1/1022.cpp
@@ -8,32 +8,25 @@ int n, tmp;
 bool b[maxn][maxn], used[maxn], fe;
 int chudu[maxn];
 
-void writeln(int x)
-{
+void writeln(int x){
 	for(int i=1;i<=n;++i)
 		if(!used[i] && b[i][x])
 			writeln(i);
-
-	if(!fe)
-		printf(" ");
+	if(!fe) printf(" ");
 	fe=false;
 
-	if(!used[x])
-	{
+	if(!used[x]){
 		printf("%d", x);
 		used[x]=true;
 	}
 }
 
-int main()
-{
+int main(){
 	memset(b, false, sizeof(b));
 	scanf("%d", &n);
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		scanf("%d", &tmp);
-		while(tmp!=0)
-		{
+		while(tmp!=0){
 			b[i][tmp]=true;
 			scanf("%d", &tmp);
 		}
@@ -45,13 +38,11 @@ int main()
 	memset(chudu, 0, sizeof(chudu));
 	for(int i=1;i<=n;++i)
 		for(int j=1;j<=n;++j)
-			if(b[i][j])
-				++chudu[i];
+			if(b[i][j]) ++chudu[i];
 
 
 	for(int i=1;i<=n;++i)
-		if(chudu[i]==0)
-			writeln(i);
+		if(chudu[i]==0) writeln(i);
 
 	printf("\n");
 	return 0;
