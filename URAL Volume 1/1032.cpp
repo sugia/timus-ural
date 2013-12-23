45ff068218df727041dea03500f08427c5cf1dde
Sugia Lau <lausugia@gmail.com>
2013-12-23 23:49:27 +0800

    Update 1032.cpp

--- URAL Volume 1/1032.cpp
+++ URAL Volume 1/1032.cpp
@@ -5,20 +5,16 @@ using namespace std;
 #define maxn 10022
 int n, a[maxn], sum[maxn]={0}, f[maxn]={0};
 
-int main()
-{
+int main(){
 	scanf("%d", &n);
 	for(int i=1;i<=n;++i)
 		scanf("%d", &a[i]);
 
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		sum[i]=(a[i]+sum[i-1])%n;
-		if(f[sum[i]] || (sum[i]==0))
-		{
+		if(f[sum[i]] || (sum[i]==0)){
 			printf("%d\n", i-f[sum[i]]);
-			for(int j=f[sum[i]]+1;j<=i;++j)
-				printf("%d\n", a[j]);
+			for(int j=f[sum[i]]+1;j<=i;++j)	printf("%d\n", a[j]);
 			break;
 		}
 		f[sum[i]]=i;
