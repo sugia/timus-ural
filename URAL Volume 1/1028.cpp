6ee592e7f5dabd9603a77a29db3717c5a36ec1e9
Sugia Lau <lausugia@gmail.com>
2013-12-23 12:16:12 +0800

    Update 1028.cpp

--- URAL Volume 1/1028.cpp
+++ URAL Volume 1/1028.cpp
@@ -6,44 +6,36 @@ const int maxn=15022;
 int c[maxx]={0}, sum[maxn]={0};
 int n;
 
-int lowbit(int x)
-{
+int lowbit(int x){
 	return x&(-x);
 }
 
-void inc(int x)
-{
-	while(x<maxx)
-	{
+void inc(int x){
+	while(x<maxx)	{
 		++c[x];
 		x+=lowbit(x);
 	}
 }
 
-int s(int x)
-{
+int s(int x){
 	int ans=0;
-	while(x>=1)
-	{
+	while(x>=1){
 		ans+=c[x];
 		x-=lowbit(x);
 	}
 	return ans;
 }
 
-int main()
-{
+int main(){
 	int x, y;
 	scanf("%d", &n);
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		scanf("%d%d", &x, &y);
 		++x;
 		++y;
 		++sum[s(x)];
 		inc(x);
 	}
-	for(int i=0;i<n;++i)
-		printf("%d\n", sum[i]);
+	for(int i=0;i<n;++i) printf("%d\n", sum[i]);
 	return 0;
 }
