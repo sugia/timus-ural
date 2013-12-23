af8406a688102482cd1929caf5c80489d54d73cf
Sugia Lau <lausugia@gmail.com>
2013-12-23 12:14:17 +0800

    Update 1025.cpp

--- URAL Volume 1/1025.cpp
+++ URAL Volume 1/1025.cpp
@@ -1,25 +1,8 @@
 #include <stdio.h>
 #define maxn 102
 int a[maxn], k, i, j, n, s;
-void qs(int s, int t);
-int main()
-{
-	scanf("%d", &n);
-	for(i=1;i<=n;++i)
-		scanf("%d", &a[i]);
 
-	qs(1, n);
-
-	s=0;
-	for(i=1;i<=n/2+1;++i)
-		s+=a[i]/2+1;
-
-	printf("%d\n", s);
-	return 0;
-}
-
-void qs(int s, int t)
-{
+void qs(int s, int t){
 	int i=s, j=t, x=a[(s+t)/2], tmp;
 	while(i<=j){
 		while(a[i]<x) ++i;
@@ -32,8 +15,21 @@ void qs(int s, int t)
 			--j;
 		}
 	}
-	if(s<j)
-		qs(s, j);
-	if(i<t)
-		qs(i, t);
+	if(s<j) qs(s, j);
+	if(i<t)	qs(i, t);
+}
+
+int main(){
+	scanf("%d", &n);
+	for(i=1;i<=n;++i)
+		scanf("%d", &a[i]);
+
+	qs(1, n);
+
+	s=0;
+	for(i=1;i<=n/2+1;++i)
+		s+=a[i]/2+1;
+
+	printf("%d\n", s);
+	return 0;
 }
