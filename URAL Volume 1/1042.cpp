532bf53c6a613a03a4352a0fe533ba3a858a41d9
Sugia Lau <lausugia@gmail.com>
2013-12-24 19:32:27 +0800

    Update 1042.cpp

--- URAL Volume 1/1042.cpp
+++ URAL Volume 1/1042.cpp
@@ -9,15 +9,12 @@ const int maxn = 251;
 int n;
 int a[maxn][maxn], g[maxn];
  
-void init()
-{
+void init(){
 	int x;
 	cin >> n;
-	for(int i = 1 ; i <= n ; i++)
-	{
+	for(int i = 1 ; i <= n ; i++){
 		cin >> x;
-		while(x != -1)
-		{
+		while(x != -1){
 			a[x][i] = 1;
 			cin >> x;
 		}
@@ -25,35 +22,29 @@ void init()
 	}
 }
  
-void work()
-{
+void work(){
 	int i, j, k, c;
-	for(i = 1 ; i <= n ; i++)
-	{
+	for(i = 1 ; i <= n ; i++){
 		for(c = i ; c <= n ; c++)if(a[c][i]) break;
 		for(k = 0 ; k <= n ; k++) swap(a[i][k], a[c][k]);
 		for(j = i + 1 ; j <= n ; j++)
-		if(a[j][i])
-		{
+		if(a[j][i]){
 			a[j][0] ^= a[i][0];
 			for(k = i ; k <= n ; k++)
 				a[j][k] ^= a[i][k];
 		}
 	}
-	for(i = n ; i >= 1 ; i--)
-	{
+	for(i = n ; i >= 1 ; i--){
 		int t = 0;
 		for(j = i + 1 ; j <= n ; j++)
 			t ^= g[j] & a[i][j];
 		g[i] = a[i][0] ^ t;
 	}
 	for(i = 1 ; i <= n ; i++)
-	if(g[i])
-		cout << i << ' ';
+	if(g[i]) cout << i << ' ';
 }
  
-int main()
-{
+int main(){
 	init();
 	work();
 	return 0;
