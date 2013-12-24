870e8d4f30e6f5b8eab942cd65a183db0596030d
Sugia Lau <lausugia@gmail.com>
2013-12-24 19:33:07 +0800

    Update 1045.cpp

--- URAL Volume 1/1045.cpp
+++ URAL Volume 1/1045.cpp
@@ -8,32 +8,25 @@ vector<int>q[1005];
 int f[1005], flag[1005];
  
 int k, ans;
-void dfs(int t)
-{
+void dfs(int t){
     flag[t] = 1;
-    if(q[t].size() == 0)
-    {
+    if(q[t].size() == 0){
         f[t] = 0; return ;
     }
-    for(int i = 0; i < q[t].size(); i ++)
-    {
-        if(flag[q[t][i]] == 0)
-        {
+    for(int i = 0; i < q[t].size(); i ++){
+        if(flag[q[t][i]] == 0){
             dfs(q[t][i]);
-            if(f[q[t][i]] == 0)
-            {
+            if(f[q[t][i]] == 0){
                 f[t] = 1;
                 if(t == k && ans > q[t][i]) ans = q[t][i];
             }
         }
     }
 }
-int main()
-{
+int main(){
     int n, i, x, y;
     scanf("%d%d", &n, &k);
-    for(i = 1; i < n; i ++)
-    {
+    for(i = 1; i < n; i ++){
         scanf("%d%d", &x, &y);
         q[x].push_back(y);
         q[y].push_back(x);
