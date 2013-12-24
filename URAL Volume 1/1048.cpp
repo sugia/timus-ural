85faff340f44e3136dddf5ecd63ae71f6e9e7949
Sugia Lau <lausugia@gmail.com>
2013-12-24 19:33:41 +0800

    Update 1048.cpp

--- URAL Volume 1/1048.cpp
+++ URAL Volume 1/1048.cpp
@@ -5,21 +5,16 @@ using namespace std;
 
 #define maxn 1000022
 int n, a[maxn], x, y;
-int main()
-{
+int main(){
 	scanf("%d", &n);
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		scanf("%d%d", &x, &y);
 		a[i]=x+y;
 	}
-
-	for(int i=n;i>1;--i)
-	{
+	for(int i=n;i>1;--i){
 		a[i-1]+=a[i]/10;
 		a[i]%=10;
 	}
-
 	for(int i=1;i<=n;++i)
 		printf("%d", a[i]);
 	printf("\n");
