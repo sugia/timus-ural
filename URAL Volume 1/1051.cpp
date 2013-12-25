323fc2067a1ce80918198dc0736512e1745c5ed5
Sugia Lau <lausugia@gmail.com>
2013-12-25 21:16:34 +0800

    Update 1051.cpp

--- URAL Volume 1/1051.cpp
+++ URAL Volume 1/1051.cpp
@@ -1,13 +1,10 @@
 //#include "stdafx.h"
 #include<stdio.h>
-int main()
-{
+int main(){
     int m,n,ans;
     scanf("%d %d",&m,&n);
-    if ((m==1)||(n==1)) 
-        ans=int((m+n)/2);
-    else 
-    {
+    if ((m==1)||(n==1)) ans=int((m+n)/2);
+    else{
         if (((m*n)%3)==0) ans=2;
         else ans=1;
     }    
