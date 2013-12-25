ff981b3c4c4889c943ab9c300a41af4510e67c1b
Sugia Lau <lausugia@gmail.com>
2013-12-25 21:17:01 +0800

    Update 1052.cpp

--- URAL Volume 1/1052.cpp
+++ URAL Volume 1/1052.cpp
@@ -6,30 +6,22 @@ using namespace std;
 #define maxn 222
 int n, x[maxn], y[maxn], ans, tmp;
 
-bool online(int a, int b, int c)
-{
+bool online(int a, int b, int c){
 	return (y[c]-y[a])*(x[b]-x[a]) == (x[c]-x[a])*(y[b]-y[a]);
 }
 
-int main()
-{
+int main(){
 	scanf("%d", &n);
-	for(int i=1;i<=n;++i)
-		scanf("%d%d", &x[i], &y[i]);
+	for(int i=1;i<=n;++i) scanf("%d%d", &x[i], &y[i]);
 
 	ans=0;
 	for(int i=1;i<n;++i)
-		for(int j=i+1;j<=n;++j)
-		{
+		for(int j=i+1;j<=n;++j){
 			tmp=0;
 			for(int k=1;k<=n;++k)
-				if(online(i, j, k))
-					++tmp;
-
-			if(ans<tmp)
-				ans=tmp;
+				if(online(i, j, k)) ++tmp;
+			if(ans<tmp) ans=tmp;
 		}
-
 	printf("%d\n", ans);
 	return 0;
 }
