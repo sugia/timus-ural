e157f1a16e0360c286ab0aa9cf9d695a60799498
Sugia Lau <lausugia@gmail.com>
2013-12-25 21:17:28 +0800

    Update 1053.cpp

--- URAL Volume 1/1053.cpp
+++ URAL Volume 1/1053.cpp
@@ -6,17 +6,13 @@ using namespace std;
 long long a, b;
 int n;
 
-inline long long gcd(long long x, long long y)
-{
-	if(x==0 || y==0)
-		return 0;
+inline long long gcd(long long x, long long y){
+	if(x==0 || y==0) return 0;
 	
-	if(x==1 || y==1)
-		return 1;
+	if(x==1 || y==1) return 1;
 
 	long long r;
-	while(y!=0)
-	{
+	while(y!=0){
 		r=x%y;
 		x=y;
 		y=r;
@@ -24,25 +20,19 @@ inline long long gcd(long long x, long long y)
 	return x;
 }
 
-int main()
-{
+int main(){
 	scanf("%d", &n);
 	scanf("%lld", &a);
-	if(n==1)
-	{
+	if(n==1){
 		printf("%lld\n", a);
 		return 0;
 	}
-
 	scanf("%lld", &b);
 	a=gcd(a, b);
-
-	for(int i=3;i<=n;++i)
-	{
+	for(int i=3;i<=n;++i){
 		scanf("%lld", &b);
 		a=gcd(a, b);
 	}
-
 	printf("%lld\n", a);
 	return 0;
 }
