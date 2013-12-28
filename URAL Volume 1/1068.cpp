f3bdb89929983495916c1cf5c89fefd65a545d26
Sugia Lau <lausugia@gmail.com>
2013-12-28 18:55:19 +0800

    Update 1068.cpp

--- URAL Volume 1/1068.cpp
+++ URAL Volume 1/1068.cpp
@@ -1,17 +1,14 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
 int ans, n;
-int main()
-{
+
+int main(){
 	cin>>n;
 	ans=0;
 	if(n>=1)
-		for(int i=1;i<=n;++i)
-			ans+=i;
+		for(int i=1;i<=n;++i) ans+=i;
 	else
-		for(int i=1;i>=n;--i)
-			ans+=i;
+		for(int i=1;i>=n;--i) ans+=i;
 	cout<<ans<<endl;
 	return 0;
 }
