146fa9e098b610b2a8f110014b5e2d14646a86a8
Sugia Lau <lausugia@gmail.com>
2013-12-28 18:51:18 +0800

    Update 1060.cpp

--- URAL Volume 1/1060.cpp
+++ URAL Volume 1/1060.cpp
@@ -8,22 +8,20 @@ bool tt=false;
  
 inline void Change(int x,int y){
 	tmp[x][y]^=1;
-	for (int i=1;i<=4;i++){
-		int fx=x+dir[i][0],fy=y+dir[i][1];
-		if (fx>0 && fx<=4 && fy>0 && fy<=4)
-			tmp[fx][fy]^=1;
+	for(int i=1;i<=4;i++){
+		int fx=x+dir[i][0], fy=y+dir[i][1];
+		if(fx>0 && fx<=4 && fy>0 && fy<=4) tmp[fx][fy]^=1;
 	}
 }
  
 void Search(int k){
 	if (k>4){
-		for (int i=1;i<=4;i++)
-			memcpy(tmp,s,sizeof tmp);
+		for(int i=1;i<=4;i++) memcpy(tmp,s,sizeof tmp);
 		memcpy(a,b,sizeof a);
 		int sum=0,j;
-		for (int i=1;i<=4;i++){
+		for(int i=1;i<=4;i++){
 			j=0;
-			while (a[i]){
+			while(a[i]){
 				j++;
 				if (a[i]&1){
 					sum++;
@@ -32,11 +30,11 @@ void Search(int k){
 				a[i]/=2;
 			}
 		}
-		for (int i=1;i<=4;i++)
-		for (int j=1;j<=4;j++)
-		if (tmp[i][j]!=tmp[1][1])	return;
+		for(int i=1;i<=4;i++)
+		for(int j=1;j<=4;j++)
+		if(tmp[i][j]!=tmp[1][1]) return;
 		tt=true;
-		if (sum<ans)	ans=sum;
+		if(sum<ans) ans=sum;
 		return;
 	}
 	for (int i=0;i<1<<4;i++){
@@ -46,17 +44,12 @@ void Search(int k){
 }
  
 int main(){
-//	freopen("s.in","r",stdin);
-//	freopen("s.out","w",stdout);
- 
 	dir[1][0]=0,dir[2][0]=0,dir[3][0]=1,dir[4][0]=-1;
 	dir[1][1]=1,dir[2][1]=-1,dir[3][1]=0,dir[4][1]=0;
  
+	for (int i=1;i<=4;i++) scanf("%s",&ch[i]);
 	for (int i=1;i<=4;i++)
-		scanf("%s",&ch[i]);
-	for (int i=1;i<=4;i++)
-	for (int j=1;j<=4;j++)
-		s[i][j]=(ch[i][j-1]=='b');
+		for (int j=1;j<=4;j++) s[i][j]=(ch[i][j-1]=='b');
  
 	ans=2134000000;
 	Search(1);
