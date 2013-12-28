f1ac0a0561c2fd028c226c58c2e7b9410a3a398b
Sugia Lau <lausugia@gmail.com>
2013-12-28 18:55:41 +0800

    Update 1069.cpp

--- URAL Volume 1/1069.cpp
+++ URAL Volume 1/1069.cpp
@@ -10,31 +10,29 @@ const int maxn=7510;
 int n=1,t,tot=0,a[maxn],e[maxn]={0},out[maxn];
 int h[maxn]={0},node[maxn<<1],next[maxn<<1];
 set<int> x;
-void add(int a,int b)
-{
+
+void add(int a,int b){
     node[++tot]=b;
     next[tot]=h[a];
     h[a]=tot;
 }
-int main()
-{
+
+int main(){
     int i,j;
     while (scanf("%d",&a[n++]) != EOF);
     --n;
-    for (i=1;i<n;++i)  ++e[a[i]];
+    for (i=1;i<n;++i) ++e[a[i]];
     for (i=1;i<=n;++i)
         if (!e[i])
             x.insert(i);
-    for (i=1;i<n;++i)
-    {
+    for (i=1;i<n;++i){
         t=*x.begin();
         x.erase(t);
         add(t,a[i]);
         add(a[i],t);
         if (!(--e[a[i]]))  x.insert(a[i]);
     }
-    for (i=1;i<=n;++i)
-    {
+    for (i=1;i<=n;++i){
         printf("%d:",i);
         for (tot=0,j=h[i];j;j=next[j])
             out[tot++]=node[j];
