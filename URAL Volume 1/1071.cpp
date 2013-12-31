fd24915911cabe9230a46d06d48f6e84020fcdf4
Sugia Lau <lausugia@gmail.com>
2013-12-31 23:41:13 +0800

    Update 1071.cpp

--- URAL Volume 1/1071.cpp
+++ URAL Volume 1/1071.cpp
@@ -6,73 +6,53 @@ using namespace std;
 vector<int> a, b;
 int x, y;
 
-void changeBase(int key, int base)
-{
-//	int tmp;
-	if(key==1)
-	{
+void changeBase(int key, int base){
+	if(key==1){
 		int xx=x;
 		a.clear();
-		while(xx!=0)
-		{
-//			tmp=xx%base;
+		while(xx!=0){
 			a.push_back(xx%base);
 			xx/=base;
 		}
 	}
-	else
-	{
+	else{
 		int yy=y;
 		b.clear();
-		while(yy!=0)
-		{
-//			tmp=yy%base;
+		while(yy!=0){
 			b.push_back(yy%base);
 			yy/=base;
 		}
 	}
 }
 
-int main()
-{
+int main(){
 	scanf("%d%d", &x, &y);
 	int maxk = x > y ? x+1 : y+1;
 	int astep, bstep;
 	bool flag=false;
-	for(int i=2;i<=maxk;++i)
-	{
-//		cout<<i<<endl;
+	for(int i=2;i<=maxk;++i){
 		changeBase(1, i);
 		changeBase(2, i);
-
 		astep=0;
 		bstep=0;
-
-		while(1)
-		{
-			if(a[astep]==b[bstep])
-			{
+		while(1){
+			if(a[astep]==b[bstep])	{
 				++astep;
 				++bstep;
 			}
-			else
-				++astep;
+			else ++astep;
 
-			if(bstep==b.size())
-				flag=true;
-			if(astep==a.size() || bstep==b.size())
-				break;
+			if(bstep==b.size()) flag=true;
+			if(astep==a.size() || bstep==b.size()) break;
 		}
 
-		if(flag)
-		{
+		if(flag){
 			printf("%d\n", i);
 			return 0;
 		}
 	}
 
-	if(!flag)
-	{
+	if(!flag){
 		printf("No solution\n");
 		return 0;
 	}
