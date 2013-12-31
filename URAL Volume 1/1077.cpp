8aae56ce49e19bf7e5dc2b7a85a6eca20dd653d4
Sugia Lau <lausugia@gmail.com>
2013-12-31 23:49:26 +0800

    Update 1077.cpp

--- URAL Volume 1/1077.cpp
+++ URAL Volume 1/1077.cpp
@@ -7,15 +7,16 @@ const int oo=301*201,maxn=202;
 int n,m,x,y,e[oo],next[oo],lis[maxn],tot,stack[maxn],l,level[maxn],p;
 bool mk[maxn];
  
-inline void Add(int x,int y)
-{	e[++l]=y;next[l]=lis[x];lis[x]=l;	}
+void Add(int x,int y){
+	e[++l]=y;next[l]=lis[x];lis[x]=l;
+}
  
 void Dfs(int u){
 	mk[u]=1;
 	int v;
 	for (int i=lis[u];i;i=next[i]){
 		v=e[i];
-		if (!mk[v])	Dfs(v);
+		if (!mk[v]) Dfs(v);
 	}
 }
  
@@ -24,7 +25,7 @@ void dfs(int u){
 	int v;
 	for (int i=lis[u];i;i=next[i]){
 		v=e[i];
-		if (!mk[v])	dfs(v);
+		if (!mk[v]) dfs(v);
 		else{
 			if (level[u]-level[v]>1){
 				printf("%d",level[u]-level[v]+1);
@@ -38,27 +39,20 @@ void dfs(int u){
 }
  
 int main(){
-	//freopen("s.in","r",stdin);
-	//freopen("s.out","w",stdout);
- 
 	scanf("%d%d",&n,&m);
- 
 	for (int i=1;i<=m;i++){
 		scanf("%d%d",&x,&y);
 		Add(x,y),Add(y,x);
 	}
  
- 
 	for (int i=1;i<=n;i++)
-	if (!mk[i])
-		p++,Dfs(i);
+	if (!mk[i]) p++, Dfs(i);
  
 	printf("%d\n",m-n+p);
 	memset(mk,0,sizeof(mk));
  
 	for (int i=1;i<=n;i++)
-	if (!mk[i])
-		dfs(i);
+	if (!mk[i]) dfs(i);
  
 	return 0;
 }
