72958662fb08d7f65086039f0b305cf1cca8e86b
Sugia Lau <lausugia@gmail.com>
2013-12-31 23:42:19 +0800

    Update 1073.cpp

--- URAL Volume 1/1073.cpp
+++ URAL Volume 1/1073.cpp
@@ -5,13 +5,10 @@ using namespace std;
 
 #define maxn 60022
 int f[maxn], n;
-int main()
-{
+int main(){
 	scanf("%d", &n);
 	memset(f, 22, sizeof(f));
 	f[0]=0;
-
-
 	for(int i=1;i<=int(sqrt(double(n)));++i)
 		for(int j=i*i;j<=n;++j)
 			f[j]= f[j] < f[j-i*i]+1 ? f[j] : f[j-i*i]+1;
