957e94bb25b402d9dd73af108353b2e82ea02da5
Sugia Lau <lausugia@gmail.com>
2013-12-31 23:40:18 +0800

    Update 1070.cpp

--- URAL Volume 1/1070.cpp
+++ URAL Volume 1/1070.cpp
@@ -1,21 +1,20 @@
 //#include "stdafx.h"
 #include<iostream>
 using namespace std;
-float min(float a,float b,float c)
-{
+float min(float a,float b,float c){
     float t=9999999;
     if ((a<t)&&(abs(a)<11)) t=a;
     if ((b<t)&&(abs(b)<11)) t=b;
     if ((c<t)&&(abs(c)<11)) t=c;
     return t;
-}    
-int deal(float x)
-{
+}
+
+int deal(float x){
     if (x+0.5>=(int)x) return((int)x+1);
     return((int)x);
-}    
-int main(void)
-{
+}
+
+int main(void){
     float a,b,x,x1,x2;
     cin>>a>>b;
     x1=min(a-b,a-b-24,a+24-b);
