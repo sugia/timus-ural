8ee3086d44c98d8023d700a271d99ac40eaa1eae
Sugia Lau <lausugia@gmail.com>
2014-01-03 20:49:35 +0800

    Update 1083.cpp

--- URAL Volume 1/1083.cpp
+++ URAL Volume 1/1083.cpp
@@ -5,20 +5,17 @@
 using namespace std;
 int n, k;
 string str;
-int main()
-{
+int main(){
 	cin>>n;
 	cin>>str;
 	k=0;
 	for(int i=0;i<str.length(); ++i)
-		if(str[i]=='!')
-			++k;
+		if(str[i]=='!') ++k;
 	unsigned long long ans=1;
 	while(n>0){
 		ans*=n;
 		n-=k;
 	}
-
 	cout<<ans<<endl;
 	return 0;
 }
