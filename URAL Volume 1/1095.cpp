00860c19214278e8b4dd256bf84864ea12c6fc52
Sugia Lau <lausugia@gmail.com>
2014-01-05 01:24:27 +0800

    Update 1095.cpp

--- URAL Volume 1/1095.cpp
+++ URAL Volume 1/1095.cpp
@@ -9,16 +9,13 @@ string s;
 int x, n;
 int b[12];
 
-int main()
-{
+int main(){
 	cin>>n;
-	for(int k=1;k<=n;++k)
-	{
+	for(int k=1;k<=n;++k){
 		memset(b, 0, sizeof(b));
 		x=0;
 		cin>>s;
-		for(int i=0;i<s.size();++i)
-			++b[int(s[i])-48];
+		for(int i=0;i<s.size();++i) ++b[int(s[i])-48];
 		--b[1];
 		--b[2];
 		--b[3];
@@ -31,14 +28,11 @@ int main()
 		x= x*10000 % 7;
 
 		for(int i=1;i<=9;++i)
-			for(int j=1;j<=b[i];++j)
-				cout<<i;
+			for(int j=1;j<=b[i];++j) cout<<i;
 		cout<<base[x];
 
-		for(int i=1;i<=b[0];++i)
-			cout<<0;
+		for(int i=1;i<=b[0];++i) cout<<0;
 		cout<<endl;
 	}
-
 	return 0;
 }
