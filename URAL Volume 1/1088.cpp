1de2b9c85efcf7668969898eb664adb78a3b12ba
Sugia Lau <lausugia@gmail.com>
2014-01-05 01:19:29 +0800

    Update 1088.cpp

--- URAL Volume 1/1088.cpp
+++ URAL Volume 1/1088.cpp
@@ -6,16 +6,15 @@
 using namespace std;
 int D, E, F, Dp, Ep, H;
 int D_a[31], E_a[31];
-int main()
-{
+int main(){
     cin >> D >> E >> F >> Dp >> Ep >> H;
     Dp --;
     Ep --;
  
     for (int i = 0; i <= F; ++i)
-        if (i >= D && i >= E && ((Dp >> i) << i) == ((Ep >> i) << i) )
-        {
+        if (i >= D && i >= E && ((Dp >> i) << i) == ((Ep >> i) << i) ){
             puts( i * 2 - D - E <= H ? "YES" : "NO" );
             return 0;
         }
+    return 0;
 }
