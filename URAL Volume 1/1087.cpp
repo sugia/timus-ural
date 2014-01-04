6387e1feb5a6b956093498914345b3b8d492b44e
Sugia Lau <lausugia@gmail.com>
2014-01-05 01:19:07 +0800

    Update 1087.cpp

--- URAL Volume 1/1087.cpp
+++ URAL Volume 1/1087.cpp
@@ -8,23 +8,18 @@ using namespace std;
 bool f[maxn];
 int k[maxm];
 
-int main()
-{
+int main(){
 	memset(f, false, sizeof(f));
 	int n, m;
 	scanf("%d%d", &n, &m);
-	for(int i=1;i<=m;++i)
-		scanf("%d", &k[i]);
+	for(int i=1;i<=m;++i) scanf("%d", &k[i]);
 
 	f[0]=true;
 	for(int i=1;i<=n;++i)
 		for(int j=1;j<=m;++j)
-			if(i>=k[j] && !f[i-k[j]])
-				f[i]=true;
+			if(i>=k[j] && !f[i-k[j]]) f[i]=true;
 
-	if(f[n])
-		printf("1\n");
-	else
-		printf("2\n");
+	if(f[n]) printf("1\n");
+	else printf("2\n");
 	return 0;
 }
