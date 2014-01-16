7c90523bb1850c8fe5e70e47d2e28e96b70f005c
Sugia Lau <lausugia@gmail.com>
2014-01-16 18:10:05 +0800

    Update 1118.cpp

--- URAL Volume 2/1118.cpp
+++ URAL Volume 2/1118.cpp
@@ -5,31 +5,26 @@ using namespace std;
 #define maxn 1000022
 int a[maxn]={0}, x, y, ans;
 long double mini=(1+maxn)/2*maxn;
-int main()
-{
+int main(){
 //	cout<<mini<<endl;
 	scanf("%d%d", &x, &y);
-	if(x>y)
-	{
+	if(x>y){
 		int tmp=x;
 		x=y;
 		y=tmp;
 	}
-	if(x==1)
-	{
+	if(x==1){
 		printf("%d\n", 1);
 		return 0;
 	}
 
-	for(int i=y;i>=x;--i)
-	{
+	for(int i=y;i>=x;--i){
 		int tmp=0;
 		for(int j=1;j<i;++j)
 			if(i%j==0)
 				tmp+=j;
 
-		if(tmp==1)
-		{
+		if(tmp==1){
 			printf("%d\n", i);
 			return 0;
 		}
@@ -38,8 +33,7 @@ int main()
 
 //		cout<<i<<' '<<t<<endl;
 
-		if(t<mini)
-		{
+		if(t<mini){
 			mini=t;
 			ans=i;
 		}
