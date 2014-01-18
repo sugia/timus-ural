f40045708843f41c1947c8de875fbab64e7030c1
Sugia Lau <lausugia@gmail.com>
2014-01-18 21:55:35 +0800

    Update 1120.cpp

--- URAL Volume 2/1120.cpp
+++ URAL Volume 2/1120.cpp
@@ -1,19 +1,13 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
-int main()
-{
+int main(){
 	int n;
 	scanf("%d", &n);
-	for(int p=int(sqrt(double(n+n))); p>0;--p)
-	{
+	for(int p=int(sqrt(double(n+n))); p>0;--p){
 		int tmp=(n+n)-p*p;
-		if(tmp%p==0 && (tmp/p)%2==1)
-		{
+		if(tmp%p==0 && (tmp/p)%2==1){
 			int a=(tmp/p+1)/2;
-
 			printf("%d %d\n", a, p);
 			return 0;
 		}
