ab34f1f3d1d5391a7d16fc5740c65e9a01b55aee
Sugia Lau <lausugia@gmail.com>
2014-01-24 18:20:39 +0800

    Update 1139.cpp

--- URAL Volume 2/1139.cpp
+++ URAL Volume 2/1139.cpp
@@ -1,17 +1,13 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
-int main()
-{
+int main(){
 	long n, m, k, s;
 	cin>>n>>m;
 	--n;
 	--m;
 	s=n+m;
-	while(m!=0)
-	{
+	while(m!=0){
 		k=n%m;
 		n=m;
 		m=k;
