79c1cac5c76d4667fd2fa0627e5f735a3e44c7c2
Sugia Lau <lausugia@gmail.com>
2014-01-27 21:59:28 +0800

    Update 1146.cpp

--- URAL Volume 2/1146.cpp
+++ URAL Volume 2/1146.cpp
@@ -1,18 +1,15 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 #define maxn 122
 int a[maxn][maxn]={0};
 
-int main()
-{
+int main(){
 	int n;
 	int r=-128;
 	scanf("%d", &n);
 	for(int i=1;i<=n;++i)
-		for(int j=1;j<=n;++j)
-		{
+		for(int j=1;j<=n;++j){
 			scanf("%d", &a[i][j]);
 			if(a[i][j]<=0 && r<a[i][j])
 				r=a[i][j];
@@ -27,15 +24,13 @@ int main()
 	for(int lef=1;lef<=n;++lef)
 		for(int rig=lef;rig<=n;++rig)
 			for(int up=1;up<=n;++up)
-				for(int dow=up;dow<=n;++dow)
-				{
+				for(int dow=up;dow<=n;++dow){
 					tmp=a[dow][rig]-a[dow][lef-1]-a[up-1][rig]+a[up-1][lef-1];
 					if(ans<tmp)
 						ans=tmp;
 				}
 
-	if(ans==0)
-		ans=r;
+	if(ans==0) ans=r;
 	printf("%d\n", ans);
 	return 0;
 }
