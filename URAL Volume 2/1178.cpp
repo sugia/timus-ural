00c16908036ed0bf1b21a4ae8a826e48f18e9e7f
Sugia Lau <lausugia@gmail.com>
2014-01-28 20:23:08 +0800

    Update 1178.cpp

--- URAL Volume 2/1178.cpp
+++ URAL Volume 2/1178.cpp
@@ -1,19 +1,15 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
 
 #define maxn 10022
 int x[maxn], y[maxn], id[maxn];
 
-void qs(int s, int t)
-{
+void qs(int s, int t){
 	int tmp;
-	if(t-s<=20)
-	{
+	if(t-s<=20){
 		for(int i=s;i<t;++i)
 			for(int j=i+1;j<=t;++j)
-				if(x[i]>x[j])
-				{
+				if(x[i]>x[j]){
 					tmp=x[i];
 					x[i]=x[j];
 					x[j]=tmp;
@@ -31,14 +27,10 @@ void qs(int s, int t)
 	}
 
 	int i=s, j=t, h=x[(s+t)/2];
-	while(i<=j)
-	{
-		while(x[i]<h)
-			++i;
-		while(h<x[j])
-			--j;
-		if(i<=j)
-		{
+	while(i<=j){
+		while(x[i]<h) ++i;
+		while(h<x[j]) --j;
+		if(i<=j){
 			tmp=x[i];
 			x[i]=x[j];
 			x[j]=tmp;
@@ -55,25 +47,20 @@ void qs(int s, int t)
 			--j;
 		}
 	}
-	if(i<t)
-		qs(i, t);
-	if(s<j)
-		qs(s, j);
+	if(i<t)	qs(i, t);
+	if(s<j)	qs(s, j);
 }
 
 
-int main()
-{
+int main(){
 	int n;
 	scanf("%d", &n);
-	for(int i=1;i<=n;++i)
-	{
+	for(int i=1;i<=n;++i){
 		scanf("%d%d", &x[i], &y[i]);
 		id[i]=i;
 	}
 
 	qs(1, n);
-	for(int i=1;i<n;i+=2)
-		printf("%d %d\n",id[i], id[i+1]);
+	for(int i=1;i<n;i+=2) printf("%d %d\n",id[i], id[i+1]);
 	return 0;
 }
