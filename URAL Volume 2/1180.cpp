36c76045126ef6a925bce693c1a3f49f7af5f629
Sugia Lau <lausugia@gmail.com>
2014-01-29 17:42:30 +0800

    Update 1180.cpp

--- URAL Volume 2/1180.cpp
+++ URAL Volume 2/1180.cpp
@@ -1,39 +1,31 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
 
 #define maxn 262
 int a[maxn], len;
-int main()
-{
+int main(){
 	len=0;
 	char ch;
-	while(cin>>ch)
-		a[++len]=int(ch-'0');
+	while(cin>>ch) a[++len]=int(ch-'0');
 
-	for(int i=len;i>=1;--i)
-	{
+	for(int i=len;i>=1;--i){
 		a[i]-=(a[i]/3)*3;
 		a[i-1]+=a[i]*10;
 	}
 
-	if(a[1]%3==0)
-	{
+	if(a[1]%3==0){
 		cout<<2<<endl;
 	}
 
-	if(a[1]%3==1)
-	{
+	if(a[1]%3==1){
 		cout<<1<<endl;
 		cout<<1<<endl;
 	}
 
-	if(a[1]%3==2)
-	{
+	if(a[1]%3==2){
 		cout<<1<<endl;
 		cout<<2<<endl;
 	}
-
 	return 0;
 }
