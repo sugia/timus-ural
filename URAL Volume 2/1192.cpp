1e50c786462876346eff970005e99c95d139dcf9
Sugia Lau <lausugia@gmail.com>
2014-01-30 18:15:19 +0800

    Update 1192.cpp

--- URAL Volume 2/1192.cpp
+++ URAL Volume 2/1192.cpp
@@ -1,16 +1,10 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 using namespace std;
-
 #define pi 3.1415926535
-
-int main()
-{
+int main(){
 	double v, a, k;
 	scanf("%lf%lf%lf", &v, &a, &k);
-//	printf("%lf %lf %lf\n", v, a, k);
-
 	double ans=v*v*sin(a*pi/90)/10*k/(k-1);
 	printf("%.2lf\n", ans);
 	return 0;
