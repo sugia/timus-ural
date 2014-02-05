085ded4979fa6633bae3737d42bbc6dd7c2a918e
Sugia Lau <lausugia@gmail.com>
2014-02-05 17:09:34 +0800

    Update 1213.cpp

--- URAL Volume 3/1213.cpp
+++ URAL Volume 3/1213.cpp
@@ -1,38 +1,27 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <string>
 using namespace std;
-
 #define maxn 32
 char name[maxn][maxn];
 int ans=-1;
-
-void ins(char *s)
-{
+void ins(char *s){
 	int i;
 	for(i=0;i<=ans;++i)
-		if(strcmp(name[i], s)==0)
-			return ;
-
+		if(strcmp(name[i], s)==0) return ;
 	strcpy(name[++ans], s);
 }
-
-int main()
-{
+int main(){
 	char s[maxn], s1[maxn], s2[maxn];
 	gets(s);
 	ins(s);
 	scanf("%s", s);
 	int i, j;
-	while(strcmp(s, "#")!=0)
-	{
-		for(i=0;s[i]!='-';++i)
-			s1[i]=s[i];
+	while(strcmp(s, "#")!=0){
+		for(i=0;s[i]!='-';++i) s1[i]=s[i];
 		s1[i]='\0';
 		++i;
 		j=i;
-		for(;s[i]!='\0';++i)
-			s2[i-j]=s[i];
+		for(;s[i]!='\0';++i) s2[i-j]=s[i];
 		s2[i-j]='\0';
 		ins(s1);
 		ins(s2);
