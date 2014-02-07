78f6dbe08ee24eda0acb40c4f975c9cb0c5b7109
Sugia Lau <lausugia@gmail.com>
2014-02-07 16:35:00 +0800

    Update 1222.cpp

--- URAL Volume 3/1222.cpp
+++ URAL Volume 3/1222.cpp
@@ -1,59 +1,38 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 #define maxn 3022
 int n, step;
 int ans[maxn];
-
-int main()
-{
+int main(){
 	scanf("%d", &n);
-	if(n==1)
-	{
+	if(n==1){
 		printf("1\n");
 		return 0;
 	}
-
 	ans[0]=1;
-	if(n%3==0)
-	{
+	if(n%3==0){
 		ans[1]=1;
 	}
-
-	if(n%3==1)
-	{
+	if(n%3==1){
 		ans[1]=4;
 		n-=4;
 	}
-
-	if(n%3==2)
-	{
+	if(n%3==2){
 		ans[1]=2;
 		n-=2;
 	}
-
-
-
 	step=n/3;
-	for(int i=1;i<=step;++i)
-	{
-		for(int j=1;j<=ans[0];++j)
-			ans[j]*=3;
-		for(int j=1;j<=ans[0]+1;++j)
-		{
+	for(int i=1;i<=step;++i){
+		for(int j=1;j<=ans[0];++j) ans[j]*=3;
+		for(int j=1;j<=ans[0]+1;++j){
 			ans[j+1]+=ans[j]/10;
 			ans[j]%=10;
 		}
-
 		int k=ans[0];
-		while(ans[k+1]!=0)
-			++k;
+		while(ans[k+1]!=0) ++k;
 		ans[0]=k;
 	}
-	
-	for(int i=ans[0];i>=1;--i)
-		printf("%d", ans[i]);
+	for(int i=ans[0];i>=1;--i) printf("%d", ans[i]);
 	printf("\n");
 	return 0;
 }
