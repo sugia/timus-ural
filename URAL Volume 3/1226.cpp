3a5f453b8c1a06ffbb215beeed4939561cd5c65a
Sugia Lau <lausugia@gmail.com>
2014-02-08 18:13:08 +0800

    Update 1226.cpp

--- URAL Volume 3/1226.cpp
+++ URAL Volume 3/1226.cpp
@@ -1,34 +1,22 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 #define maxn 262
 char c[maxn], ch;
 int top=0;
 bool flag;
-
-int main()
-{
-	while(scanf("%c", &ch)!=EOF)
-	{
+int main(){
+	while(scanf("%c", &ch)!=EOF){
 		flag=false;
-		if(isalpha(ch))
-			c[++top]=ch;
-		else
-		{
-			for(int i=top;i>0;--i)
-				printf("%c", c[i]);
+		if(isalpha(ch))	c[++top]=ch;
+		else{
+			for(int i=top;i>0;--i) printf("%c", c[i]);
 			printf("%c", ch);
 			top=0;
 			flag=true;
 		}
 	}
-
-	if(!flag)
-	{
-		for(int i=top;i>0;--i)
-			printf("%c", c[i]);
+	if(!flag){
+		for(int i=top;i>0;--i) printf("%c", c[i]);
 	}
-
 	return 0;
 }
