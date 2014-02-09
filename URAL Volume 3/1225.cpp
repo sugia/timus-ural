b75f9fcf14251c4d90e0447ab5371cb10c5c1015
Sugia Lau <lausugia@gmail.com>
2014-02-09 21:09:49 +0800

    Update 1225.cpp

--- URAL Volume 3/1225.cpp
+++ URAL Volume 3/1225.cpp
@@ -1,19 +1,13 @@
-//#include "stdafx.h"
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-
 #define maxn 52
 unsigned long long f[maxn], n;
-int main()
-{
+int main(){
 	scanf("%d", &n);
 	f[1]=2;
 	f[2]=2;
-	
-	for(int i=3;i<=n;++i)
-		f[i]=f[i-1]+f[i-2];
-
+	for(int i=3;i<=n;++i) f[i]=f[i-1]+f[i-2];
 	printf("%lld\n", f[n]);
 	return 0;
 }
