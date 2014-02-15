fb724e3fb61467363615d64e2fba9d41a2accc9a
Sugia Lau <lausugia@gmail.com>
2014-02-15 21:39:39 +0800

    Update 1313.cpp

--- URAL Volume 4/1313.cpp
+++ URAL Volume 4/1313.cpp
@@ -1,17 +1,13 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <string.h>
 #include <string>
 using namespace std;
 const int maxn=102;
 int n, a[maxn][maxn];
-int main()
-{
+int main(){
 	cin>>n;
 	for(int i=1;i<=n;++i)
-		for(int j=1;j<=n;++j)
-		cin>>a[i][j];
-
+		for(int j=1;j<=n;++j) cin>>a[i][j];
 	cout<<a[1][1];
 	int i, j;
 	for(int k=2;k<=n;++k){
@@ -23,7 +19,6 @@ int main()
 			++j;
 		}
 	}
-
 	for(int k=2;k<=n;++k){
 		i=n;
 		j=k;
