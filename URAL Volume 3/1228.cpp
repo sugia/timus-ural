837b2ee61521e7f6a31b56fadff7e8012b553f52
Sugia Lau <lausugia@gmail.com>
2014-02-17 18:21:45 +0800

    Update 1228.cpp

--- URAL Volume 3/1228.cpp
+++ URAL Volume 3/1228.cpp
@@ -1,16 +1,12 @@
-//#include "stdafx.h"
 #include <iostream>
 #include<stdio.h>
- 
-int main()
-{ 
+int main(){ 
     long long n, s, a, b;
 	scanf("%lld%lld", &n, &s);
 	scanf("%lld", &a);
 	printf("%lld", s/a-1);
-		--n;
-	while(n>0)
-	{
+	--n;
+	while(n>0){
 		scanf("%lld", &b);
 		printf(" %lld", a/b-1);
 		a=b;
@@ -18,5 +14,4 @@ int main()
 	}
 	printf("\n");
 	return 0;
-
 }
