04112fe153687c0030a5465a639aaa8f35a37b83
Sugia Lau <lausugia@gmail.com>
2014-02-26 22:52:45 +0800

    Update 1327.cpp

--- URAL Volume 4/1327.cpp
+++ URAL Volume 4/1327.cpp
@@ -1,16 +1,11 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 int a, b, ans;
-int main()
-{
+int main(){
 	cin>>a>>b;
 	ans=0;
 	for(int i=a;i<=b;++i)
-		if(i%2==1)
-			++ans;
-
+		if(i%2==1) ++ans;
 	cout<<ans<<endl;
 	return 0;
 }
