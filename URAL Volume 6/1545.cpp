011d9a3da5f48eae239e5b97f60c1a071c7464b2
Sugia Lau <lausugia@gmail.com>
2014-03-01 17:35:07 +0800

    Update 1545.cpp

--- URAL Volume 6/1545.cpp
+++ URAL Volume 6/1545.cpp
@@ -1,24 +1,16 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <string.h>
 #include <string>
 using namespace std;
-
 #define maxn 1022
 int n;
 string str[maxn];
 char ch;
-int main()
-{
+int main(){
 	cin>>n;
-	for(int i=1;i<=n;++i)
-		cin>>str[i];
-
+	for(int i=1;i<=n;++i) cin>>str[i];
 	cin>>ch;
-
-
 	for(int i=1;i<=n;++i)
-		if(str[i][0]==ch)
-			cout<<str[i]<<endl;;
+	    if(str[i][0]==ch) cout<<str[i]<<endl;;
 	return 0;
 }
