8f219a3048e6f8608c37135b33e49313732e7fae
Sugia Lau <lausugia@gmail.com>
2014-03-05 00:09:07 +0800

    Update 1353.cpp

--- URAL Volume 4/1353.cpp
+++ URAL Volume 4/1353.cpp
@@ -1,19 +1,13 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 #define maxn 82
 long a[maxn];
-
-int main()
-{
+int main(){
 	a[0]=1;
 	for(int i=1;i<=9;++i)
 		for(int j=9*i;j>=1;--j)
-			for(int k=1;k<=9;++k)
-			{
-				if(k>j)
-					break;
+			for(int k=1;k<=9;++k){
+				if(k>j) break;
 				a[j]+=a[j-k];
 			}
 	++a[1];
