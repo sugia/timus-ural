2a9fa42052be9f62f4e95e05f1a136d79021d3eb
Sugia Lau <lausugia@gmail.com>
2014-03-06 12:41:35 +0800

    Update 1433.cpp

--- URAL Volume 5/1433.cpp
+++ URAL Volume 5/1433.cpp
@@ -1,9 +1,5 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
-
-
 int main(){
 	char a, b, c, d;
 	scanf("%c%c%c%c", &a, &b, &c, &d);
@@ -11,46 +7,28 @@ int main(){
 	scanf("%c", &ch);
 	char w, x, y, z;
 	scanf("%c%c%c%c", &w, &x, &y, &z);
-
 	bool flag=false;
 	if(a==w){
-		if(b==x && c==y && d==z)
-			flag=true;
-		if(c==x && d==y && b==z)
-			flag=true;
-		if(d==x && b==y && c==z)
-			flag=true;
+		if(b==x && c==y && d==z) flag=true;
+		if(c==x && d==y && b==z) flag=true;
+		if(d==x && b==y && c==z) flag=true;
 	}
-
 	if(b==w){
-		if(a==x && d==y && c==z)
-			flag=true;
-		if(d==x && c==y && a==z)
-			flag=true;
-		if(c==x && a==y && d==z)
-			flag=true;
+		if(a==x && d==y && c==z) flag=true;
+		if(d==x && c==y && a==z) flag=true;
+		if(c==x && a==y && d==z) flag=true;
 	}
-
 	if(c==w){
-		if(b==x && d==y && a==z)
-			flag=true;
-		if(d==x && a==y && b==z)
-			flag=true;
-		if(a==x && b==y && d==z)
-			flag=true;
+		if(b==x && d==y && a==z) flag=true;
+		if(d==x && a==y && b==z) flag=true;
+		if(a==x && b==y && d==z) flag=true;
 	}
-
 	if(d==w){
-		if(c==x && b==y && a==z)
-			flag=true;
-		if(b==x && a==y && c==z)
-			flag=true;
-		if(a==x && c==y && b==z)
-			flag=true;
+		if(c==x && b==y && a==z) flag=true;
+		if(b==x && a==y && c==z) flag=true;
+		if(a==x && c==y && b==z) flag=true;
 	}
-	if(flag)
-		printf("equal\n");
-	else
-		printf("different\n");
+	if(flag) printf("equal\n");
+	else printf("different\n");
 	return 0;
 }
