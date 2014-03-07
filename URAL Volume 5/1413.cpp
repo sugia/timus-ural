3ebe1a208742e29b64e00d51212f26ed1a59ea3c
Sugia Lau <lausugia@gmail.com>
2014-03-08 00:17:55 +0800

    Update 1413.cpp

--- URAL Volume 5/1413.cpp
+++ URAL Volume 5/1413.cpp
@@ -1,61 +1,38 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
 const double k=sqrt(double(2))/2;
 int tmp;
-double x, y;
-int main()
-{
-	x=0, y=0;
-	char ch;
-	while(cin>>ch)
-	{
+double x=0, y=0;
+char ch;
+int main(){
+	while(cin>>ch){
 		tmp=int(ch-'0');
-		if(tmp==0)
-		{
+		if(tmp==0){
 			printf("%.10lf %.10lf\n", x, y);
 			return 0;
 		}
-		else
-			if(tmp==1)
-			{
+		else if(tmp==1){
 				x-=k;
 				y-=k;
+		}
+		else if(tmp==2) --y;
+		else if(tmp==3){
+				x+=k;
+				y-=k;
 			}
-			else
-				if(tmp==2)
-					--y;
-				else
-					if(tmp==3)
-					{
-						x+=k;
-						y-=k;
-					}
-					else
-						if(tmp==4)
-							--x;
-						else
-							if(tmp==6)
-								++x;
-							else
-								if(tmp==7)
-								{
-									x-=k;
-									y+=k;
-								}
-								else
-									if(tmp==8)
-										++y;
-									else
-										if(tmp==9)
-										{
-											x+=k;
-											y+=k;
-										}
+		else if(tmp==4)	--x;
+		else if(tmp==6) ++x;
+		else if(tmp==7){
+			x-=k;
+			y+=k;
+		}
+		else if(tmp==8)	++y;
+		else if(tmp==9){
+			x+=k;
+			y+=k;
+		}
 	}
-
 	printf("%.10lf %.10lf\n", x, y);
 	return 0;
 }
