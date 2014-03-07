b76a7c8cfe0b105fa9fc871f170697e527851348
Sugia Lau <lausugia@gmail.com>
2014-03-07 13:13:14 +0800

    Update 1490.cpp

--- URAL Volume 5/1490.cpp
+++ URAL Volume 5/1490.cpp
@@ -1,24 +1,16 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
-long long sqr(long long x)
-{
+long long sqr(long long x){
 	return x*x;
 }
-
-int main()
-{
+int main(){
 	long long r, x, y, ans=0;
 	scanf("%lld", &r);
 	x=r;
-	for(y=1;y<=r;++y)
-	{
-		while(sqr(x-1) + sqr(y-1) >= sqr(r))
-			--x;
+	for(y=1;y<=r;++y){
+		while(sqr(x-1) + sqr(y-1) >= sqr(r)) --x;
 		ans+=x;
 	}
-
 	printf("%lld\n", ans*4);
 	return 0;
 }
