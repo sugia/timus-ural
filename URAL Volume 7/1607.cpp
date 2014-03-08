34bbca5efbe0851e25b9b3610d75a80adf9b395d
Sugia Lau <lausugia@gmail.com>
2014-03-08 23:03:17 +0800

    Update 1607.cpp

--- URAL Volume 7/1607.cpp
+++ URAL Volume 7/1607.cpp
@@ -1,35 +1,24 @@
-//#include "stdafx.h"
 #include <iostream>
 #include <stdio.h>
 using namespace std;
-
 int a, b, c, d;
 bool flag=true;
-int main()
-{
+int main(){
 	scanf("%d%d%d%d", &a, &b, &c, &d);
-	if(a>=c)
-	{
+	if(a>=c){
 		printf("%d\n", a);
 		return 0;
 	}
-
-	while(a<c && a+b<=c)
-	{
+	while(a<c && a+b<=c){
 		a+=b;
 		flag=false;
-		if(a<=c-d)
-		{
+		if(a<=c-d){
 			c-=d;
 			flag=true;
 		}
-		if(!flag)
-			break;
+		if(!flag) break;
 	}
-
-	if(flag)
-		printf("%d\n", c);
-	else
-		printf("%d\n", a);
+	if(flag) printf("%d\n", c);
+	else printf("%d\n", a);
 	return 0;
 }
