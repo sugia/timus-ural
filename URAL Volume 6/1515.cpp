9f6a93cec8feeb1cdd0307ca5bbe2ebc0e6d4f42
Sugia Lau <lausugia@gmail.com>
2014-03-11 00:17:01 +0800

    Update 1515.cpp

--- URAL Volume 6/1515.cpp
+++ URAL Volume 6/1515.cpp
@@ -1,26 +1,17 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 #define maxn 122
 int a[maxn], n;
-
-int main()
-{
+int main(){
 	scanf("%d", &n);
-	for(int i=0;i<n;++i)
-		scanf("%d", &a[i]);
-
+	for(int i=0;i<n;++i) scanf("%d", &a[i]);
 	int ans=0;
 	for(int i=0;i<n;++i)
-		if(a[i]>ans+1)
-		{
+		if(a[i]>ans+1){
 			printf("%d\n", ans+1);
 			return 0;
 		}
-		else
-			ans+=a[i];
-
+		else ans+=a[i];
 	printf("%d\n", ans+1);
 	return 0;
 }
