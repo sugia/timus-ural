541c0f117eba66368139c7718cb00d3c6990b90c
Sugia Lau <lausugia@gmail.com>
2014-03-17 00:17:20 +0800

    Update 1537.cpp

--- URAL Volume 6/1537.cpp
+++ URAL Volume 6/1537.cpp
@@ -1,21 +1,15 @@
-//#include "stdafx.h"
 #include <iostream>
 using namespace std;
-
 const int maxn=10000022;
 int f[maxn];
-
-int main()
-{
+int main(){
 	int k, p;
 	scanf("%d%d", &k, &p);
 	f[1]=0;
 	f[2]=1;
-	for(int i=3;i<=k;++i)
-	{
+	for(int i=3;i<=k;++i){
 		f[i]=f[i-1];
-		if(i&1^1)
-			f[i]=(f[i]+f[i>>1])%p;
+		if(i&1^1) f[i]=(f[i]+f[i>>1])%p;
 	}
 	printf("%d\n", f[k]%p);
 	return 0;
