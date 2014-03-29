af0a396fa49ff4c6493f87eb109cf7a56d9e6758
Sugia Lau <lausugia@gmail.com>
2014-03-29 17:07:15 +0800

    Update 1224.cpp

--- URAL Volume 3/1224.cpp
+++ URAL Volume 3/1224.cpp
@@ -1,25 +1,18 @@
-//#include "stdafx.h"
 #include <stdio.h>
 #include <iostream>
 using namespace std;
-
 long long n, m;
 int f[4][4]={0};
-
-int main()
-{
+int main(){
 	f[1][1]=0;
 	f[1][2]=0;
 	f[1][3]=0;
-
 	f[2][1]=1;
 	f[2][2]=2;
 	f[2][3]=2;
-
 	f[3][1]=1;
 	f[3][2]=3;
 	f[3][3]=4;
-
 	long long ans=0;
 	scanf("%lld%lld", &n, &m);
 	long long mini=min(n, m);
@@ -27,23 +20,14 @@ int main()
 	ans=tmp*4;
 	n-=tmp*2;
 	m-=tmp*2;
-
-	if(n>3)
-	{
-		if(m==1)
-			++ans;
-		if(m==2)
-			ans+=3;
+	if(n>3){
+		if(m==1) ++ans;
+		if(m==2) ans+=3;
 	}
-	else
-		if(m>3)
-		{
-			if(n==2)
-				ans+=3;
-		}
-		else
-			ans+=f[n][m];
-
+	else if(m>3){
+		if(n==2) ans+=3;
+	}
+	else ans+=f[n][m];
 	printf("%lld\n", ans);
 	return 0;
 }
